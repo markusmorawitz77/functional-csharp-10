15e7ae88788e73bd308bbdc2919b679783f318fc
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-25 18:33:50 +0200

    follow course

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -6,14 +6,11 @@ public static class DateTimeExtensions
 {
     public static Year ToYear(this DateTime time) => new(time.Year);
 
-    public static IEnumerable<Month> GetYearMonths(this DateTime time) => 
-        time.Year.GetYearMonths();
-
     public static IEnumerable<Month> GetDecadeMonths(this DateTime time) =>
-        Enumerable.Range(time.Year.ToDecadeBeginning(), 10).SelectMany(GetYearMonths);
+        Enumerable.Range(time.Year.ToDecadeBeginning(), 10)
+            .Select(year => new Year(year))
+            .SelectMany(year => year.Months);
 
     private static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
 
-    public static IEnumerable<Month> GetYearMonths(this int year) =>
-        Enumerable.Range(1,12).Select(month => new Month(new(year), month));
 }
--- Models/Year.cs
+++ Models/Year.cs
@@ -5,4 +5,10 @@ public record struct Year(int Number)
     public Month GetMonth(int ordinal) => 
         ordinal >= 1 && ordinal <= 12 ? new(this, ordinal)
         : throw new ArgumentOutOfRangeException(nameof(ordinal));
-}
+
+    
+    public IEnumerable<Month> Months => this.GetMonths(this);
+    
+    private IEnumerable<Month> GetMonths(Year year) =>
+        Enumerable.Range(1,12).Select(ordninal => new Month(year, ordninal));
+}   
