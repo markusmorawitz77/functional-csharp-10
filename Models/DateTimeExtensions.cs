2f6abd9a3ed9c6c18ffde65af8ce214fa4f68543
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-25 18:10:50 +0200

    follow course

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -1,16 +1,18 @@
 ﻿using System.Linq;
 namespace Models;
 
+public record Month(int Year, int Ordinal);
+
 public static class DateTimeExtensions
 {
-    public static IEnumerable<(int year, int month)> GetYearMonths(this DateTime time) => 
+    public static IEnumerable<Month> GetYearMonths(this DateTime time) => 
         time.Year.GetYearMonths();
 
-    public static IEnumerable<(int year, int month)> GetDecadeMonths(this DateTime time) =>
+    public static IEnumerable<Month> GetDecadeMonths(this DateTime time) =>
         Enumerable.Range(time.Year.ToDecadeBeginning(), 10).SelectMany(GetYearMonths);
 
     private static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
 
-    public static IEnumerable<(int year, int month)> GetYearMonths(this int year) =>
-        Enumerable.Range(1,12).Select(month => (year, month));
+    public static IEnumerable<Month> GetYearMonths(this int year) =>
+        Enumerable.Range(1,12).Select(month => new Month(year, month));
 }
