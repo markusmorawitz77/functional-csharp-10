3af15be9f7ba51558c462177c32be3854729db75
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-28 07:53:16 +0200

    follow course

--- Models/Media/BarcodeGeneration.cs
+++ Models/Media/BarcodeGeneration.cs
@@ -6,37 +6,12 @@ namespace Models.Media;
 
 public static class BarcodeGeneration
 {
-    public static FileContent ToCode39(this StockKeepingUnit sku, int barHeight)
-    {
-        float horizontalMargin = 5;
-        float verticalMargin = 3;
-        float padding = 2.0f;
+    public static FileContent ToCode39(this StockKeepingUnit sku, int barHeight) =>
+        sku.Value.ToCode39Bars().ToCode39Bitmap(barHeight).ToPng();
 
-        SKPaint[] bars = new[]
-        {
-            ThinBar, Space, ThinBar, ThickBar, ThickBar, ThinBar,
-            ThickBar, ThinBar, ThinBar, Space, ThickBar, ThinBar,
-            ThinBar, ThickBar, ThinBar, Space, ThickBar, ThinBar,
-            ThinBar, Space, ThinBar, ThickBar, ThickBar, ThinBar,
-        };
+    private static IEnumerable<int> ToCode39Bars(this string value) => Enumerable.Empty<int>();
 
-        int height = (int)Math.Ceiling(barHeight + verticalMargin * 2);
-        int width = (int)Math.Ceiling(bars.Sum(bar => bar.StrokeWidth) + (bars.Length - 1) * padding + horizontalMargin * 2);
-
-        SKBitmap bitmap = new(width, height);
-        SKCanvas canvas = new(bitmap);
-        canvas.Clear(SKColors.White);
-
-        float x = horizontalMargin;
-        foreach (SKPaint bar in bars)
-        {
-            float lineX = x + bar.StrokeWidth / 2;
-            canvas.DrawLine(lineX, verticalMargin, lineX, barHeight + verticalMargin, bar);
-            x += bar.StrokeWidth + padding;
-        }
-
-        return bitmap.ToPng();
-    }
+    private static SKBitmap ToCode39Bitmap(this IEnumerable<int> bars, int barHeight) => new();
 
     private static FileContent ToPng(this SKBitmap bitmap) =>
         new(bitmap.Encode(SKEncodedImageFormat.Png, 100).ToArray(), "image/png");
