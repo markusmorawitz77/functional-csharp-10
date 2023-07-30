018543c637f381afecda4d0f0055bb6f2b33e7bd
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 11:00:20 +0200

    follow course

--- Models/Types/Components/Part.cs
+++ Models/Types/Components/Part.cs
@@ -1,3 +1,10 @@
+using Models.Types.Common;
+
 namespace Models.Types.Components;
 
-public record Part(Guid Id, string Name, StockKeepingUnit Sku);
+public abstract record InventoryItem(Guid Id, string Name, StockKeepingUnit Sku);
+public record Part(Guid Id, string Name, StockKeepingUnit Sku)
+    : InventoryItem(Id, Name, Sku);
+
+public record Matirial(Guid Id, string Name, StockKeepingUnit Sku, Measure Quantity)
+    : InventoryItem(Id, Name, Sku);
