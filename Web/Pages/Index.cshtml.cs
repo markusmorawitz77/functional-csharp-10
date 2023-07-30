4b3e0d40528dd4acadc6e6c0f3f49898e597a346
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 12:17:05 +0200

    follow course

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Models.Types.Components;
 using Models.Types.Common;
 using Models.Types.Products;
+using Microsoft.AspNetCore.Http.Features;
+using System.Data;
 
 namespace Web.Pages;
 
@@ -24,6 +26,15 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-        this.AllProducts = this.Specifications.GetAll();
+        this.AllProducts = this.Specifications.GetAll().Where(this.IsSupported);
     }
+
+    private bool IsSupported(AssemblySpecification spec) =>
+        spec.Components
+            .Join(this.Inventory.GetAll(),
+                component => component.part.Id, item => item.part.Id,
+                (c,i) => (part: c.part, requested: c.quantity, available: i.quantity))
+            .Where(row => row.requested.Unit == row.available.Unit)
+            .All(row => row.requested.Value <= row.available.Value);
+
 }
