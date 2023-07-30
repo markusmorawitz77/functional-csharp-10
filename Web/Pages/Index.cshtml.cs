dd63cd7dd33f88719358e52f6ef9956f7c5351a5
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 12:10:35 +0200

    follow course

--- Web/Pages/Index.cshtml.cs
+++ Web/Pages/Index.cshtml.cs
@@ -9,11 +9,14 @@ namespace Web.Pages;
 public class IndexModel : PageModel
 {
     IReadOnlyRepository<AssemblySpecification> Specifications { get; }
+    IReadOnlyRepository<(Part part, DiscreteMeasure quantity)> Inventory { get; }
 
     public IndexModel(
-        IReadOnlyRepository<AssemblySpecification> parts)
+        IReadOnlyRepository<AssemblySpecification> parts,
+        IReadOnlyRepository<(Part part, DiscreteMeasure quantity)> inventory)
     {
         this.Specifications = parts;
+        this.Inventory = inventory;
     }
 
     public IEnumerable<AssemblySpecification> AllProducts { get; set; }
