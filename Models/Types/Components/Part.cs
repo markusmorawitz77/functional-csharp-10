fe1ba3911653eccca504222ac2f1fd669498078e
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 11:04:42 +0200

    follow course

--- Models/Types/Components/Part.cs
+++ Models/Types/Components/Part.cs
@@ -6,6 +6,25 @@ public abstract record InventoryItem(Guid Id, string Name, StockKeepingUnit Sku)
 public record Part(Guid Id, string Name, StockKeepingUnit Sku)
     : InventoryItem(Id, Name, Sku);
 
-public record Matirial(Guid Id, string Name, StockKeepingUnit Sku,
+public record Material(Guid Id, string Name, StockKeepingUnit Sku,
     ContinuousMeasure Quantity)
     : InventoryItem(Id, Name, Sku);
+
+public static class InventoryItemExtensions
+{
+    public static InventoryItem AsDiscriminatedUnion(this InventoryItem item) =>
+        item switch
+        {
+            Part or Material => item,
+            _ => throw new ArgumentException("InventoryItem must be either Part or Material")
+        };
+    public static TResult MapAny<TResult>(this InventoryItem item,
+        Func<Part, TResult> part,
+        Func<Material, TResult> material) =>
+        item.AsDiscriminatedUnion() switch
+        {
+            Part p => part(p),
+            Material m => material(m),
+            _ => default!
+        };
+}
