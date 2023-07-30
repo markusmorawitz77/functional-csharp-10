52f74113a5b4b084f98b1cf0045d5dfa9e0eb3e2
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 10:26:15 +0200

    follow course

--- Models/Common/MeasureTransform.cs
+++ Models/Common/MeasureTransform.cs
@@ -8,10 +8,17 @@ public static class MeasureTransforms
     public static (Measure a, Measure b) SplitInHalves(this Measure m) => 
     m switch
     {
-        DiscreteMeasure d =>
-            (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 }),
-        ContinuousMeasure c => 
-            (c with { Value = c.Value / 2 }, c with { Value = c.Value / 2 }),
+        DiscreteMeasure d => SplitInHalves(d),
+        ContinuousMeasure c => SplitInHalves(c),
         _ => throw new ArgumentException("Measure must be either DiscreteMeasure or ContinuousMeasure")
     };
+
+    private static (Measure a, Measure b) SplitInHalves(this DiscreteMeasure d) =>
+        (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 });
+
+    private static (Measure a, Measure b) SplitInHalves(this ContinuousMeasure c)
+    {
+        Measure half = c with { Value = c.Value / 2 };
+        return (half, half);
+    }
 }
