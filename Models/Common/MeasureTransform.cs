8c5febd290c1adc3e43f102d912dccae42c26464
Markus Morawitz <markus.morawitz@gmx.com>
2023-07-30 10:20:14 +0200

    follow course

--- Models/Common/MeasureTransform.cs
+++ Models/Common/MeasureTransform.cs
@@ -5,5 +5,12 @@ namespace Models.Common;
 
 public static class MeasureTransforms
 {
-    public static (Measure a, Measure b) SplitInHalves(this Measure m) => (m, m);
+    public static (Measure a, Measure b) SplitInHalves(this Measure m) => 
+    m switch
+    {
+        DiscreteMeasure d =>
+            (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 }),
+        ContinuousMeasure c => 
+            (c with { Value = c.Value / 2 }, c with { Value = c.Value / 2 })
+    };
 }
